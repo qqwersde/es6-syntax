bf25314ee297fe64cdd5ce2d17b0f4ef71d4ee6c
Zhonghao Yin <zhonghao.yin@thoughtworks.com>
2020-06-17 20:56:07 +0800

    using for of and forEach to get index of column to implement

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,34 @@
-const parseData
+const parseData = function (input) {
+    let {data,column}=input;
+    const array=[];
+    /*const result = data.map((v)=>{
+
+      const obj = {};
+      column.forEach((b,index)=>{
+        obj[b.name] = v[index]
+      })
+      array.push(obj);
+    });*/
+
+
+    for(let v of data){
+
+    const obj = {};
+    column.forEach((b,index)=>{
+      obj[b.name] = v[index]
+    })
+      array.push(obj);
+     }
+
+  /*  for(let v of data){
+
+        const obj={};
+        for (let [index, elem] of column.entries()) {
+            obj[elem.name]=v[index]
+        }
+        array.push(obj);
+    }*/
+    return array;
+}
+
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,14 @@
-const inject
+const inject=(items, sections)=>{
+    for (let v of sections){
+        const con=v.content;
+        const ind=v.index;
+        if(ind==0){
+            items.splice(ind,0,con);
+        }
+        else if(ind>0){
+            items.splice(ind+1,0,con);
+        }
+    }
+    return items;
+}
 export { inject };
